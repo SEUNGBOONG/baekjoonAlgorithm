7866621ee4dd36919475429fa3c7342d3f0ab13c
승엽 <tmdduqflfl@naver.com>
2024-07-15 16:42:09 +0900

    7/15 코테

---
+++ 스택/제로.java
@@ -0,0 +1,33 @@
+package 스택;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class 제로 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            int number = sc.nextInt();
+            if (number == 0) {
+                stack.pop();
+            } else {
+                stack.add(number);
+            }
+        }
+
+        if (stack.isEmpty()) {
+            System.out.println(0);
+            return;
+        }
+        int sum = 0;
+        while (!stack.isEmpty()) {
+            int num = stack.pop();
+            sum += num;
+        }
+        System.out.println(sum);
+    }
+}
