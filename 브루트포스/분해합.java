d6cb586a9b97c402488718c0fed70b0c5bb53be4
승엽 <tmdduqflfl@naver.com>
2025-01-03 17:45:00 +0900

    1/3 코테 분해합

---
+++ 브루트포스/분해합.java
@@ -0,0 +1,32 @@
+package 브루트포스;
+
+import java.util.Scanner;
+
+public class 분해합 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(new 분해합().solution(number));
+    }
+
+    public int solution(int number) {
+        for (int i = 1; i < number; i++) { // 1부터 number - 1까지 검사
+            int sum = i;
+            int temp = i;
+
+            // 각 자릿수 더하기
+            while (temp > 0) {
+                sum += temp % 10; // 마지막 자릿수 더하기
+                temp /= 10;       // 마지막 자릿수 제거
+            }
+
+            // 분해합이 주어진 숫자와 같은 경우
+            if (sum == number) {
+                return i;
+            }
+        }
+
+        // 생성자가 없는 경우
+        return 0;
+    }
+}
