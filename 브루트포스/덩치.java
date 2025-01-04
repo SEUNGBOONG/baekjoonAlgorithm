a353b94054b8354b0fec3a00af99bfac21087959
승엽 <tmdduqflfl@naver.com>
2025-01-05 03:23:51 +0900

    1/5 덩치

--- 브루트포스/덩치.java
+++ 브루트포스/덩치.java
@@ -15,19 +15,18 @@ public class 덩치 {
         int[][] arr = new int[N][2];
         String[] str;
 
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             str = br.readLine().split(" ");
-            arr[i][0] = Integer.parseInt(str[0]);	// [i][0] : 몸무게
-            arr[i][1] = Integer.parseInt(str[1]);	// [i][1] : 키
+            arr[i][0] = Integer.parseInt(str[0]);    // [i][0] : 몸무게
+            arr[i][1] = Integer.parseInt(str[1]);    // [i][1] : 키
         }
-
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             int rank = 1;
 
-            for(int j = 0; j < N; j++) {
-                if(i == j) continue;
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
                 if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
                     rank++;
                 }
