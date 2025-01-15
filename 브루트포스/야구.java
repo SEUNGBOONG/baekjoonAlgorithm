1bfbf3bf863c30d12f046e44684350b0eb832574
승엽 <tmdduqflfl@naver.com>
2025-01-15 17:04:17 +0900

    1/15 야구

---
+++ 브루트포스/야구.java
@@ -0,0 +1,24 @@
+package 브루트포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 야구 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int[] score = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int result = 0;
+            for (int j = 0; j < 9; j++) {
+
+            }
+
+        }
+
+    }
+}
