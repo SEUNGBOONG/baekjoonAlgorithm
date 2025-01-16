5e85e75e04ef0993f70ae782d7f6625eeee4e247
승엽 <tmdduqflfl@naver.com>
2025-01-16 17:05:21 +0900

    1/16 코테 실패 다시풀기

---
+++ 브루트포스/연산자끼워넣기.java
@@ -0,0 +1,30 @@
+package 브루트포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 연산자끼워넣기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] numbers = new int[n];
+
+        // 입력: 숫자 배열
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 입력: 연산자 개수
+        int[] operators = new int[4];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) {
+            operators[i] = Integer.parseInt(st.nextToken());
+        }
+
+    }
+
+}
