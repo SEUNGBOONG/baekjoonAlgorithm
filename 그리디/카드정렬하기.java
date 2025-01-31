847e4d938858071107b237f7441b9c6b0118af57
승엽 <tmdduqflfl@naver.com>
2025-01-31 20:23:02 +0900

    1/31 가비아 코테 그리디 준비 우선순위큐 카드정렬2

--- 그리디/카드정렬하기.java
+++ 그리디/카드정렬하기.java
@@ -1,47 +1,28 @@
 package 그리디;
 
 import java.util.*;
-import java.util.Scanner;
 
 public class 카드정렬하기 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int N = sc.nextInt();
-        int temp = 0;
-        int result = 0;
-        List<Integer> list = new ArrayList<>();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for (int i = 0; i < N; i++) {
-            list.add(sc.nextInt());
+            pq.add(sc.nextInt());
         }
 
-        //10 30 40
-        while (true) {
-            Collections.sort(list);
-            int i = 0;
-            int tmp = 0;
-            if (list.size()>1){
-                if (list.get(i) + list.get(i + 1) < list.get(i + 2) &&
-                        list.get(i) + list.get(i + 1) == list.get(i + 2) ) {
-                    tmp = list.get(i) + list.get(i + 1);
-                    list.remove(i);
-                    list.remove(i+1);
-                    list.add(tmp);
-                    result += tmp;
-                }
-            }
+        int result = 0;
 
-            else{
-                tmp+=list.get(i)+list.get(i+1);
-                result += tmp;
-                System.out.println(result);
-                break;
-
-            }
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+            int sum = a + b;
+            result += sum;
+            pq.add(sum);
         }
+
         System.out.println(result);
-
     }
-
 }
