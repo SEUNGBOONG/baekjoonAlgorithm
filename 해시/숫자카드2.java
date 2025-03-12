d4437f5904b9fb2ecbf94395a0d9356ec3c446aa
승엽 <tmdduqflfl@naver.com>
2025-03-12 16:10:03 +0900

    3/12 해시로푸는것

--- 해시/숫자카드2.java
+++ 해시/숫자카드2.java
@@ -1,39 +1,33 @@
 package 해시;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class 숫자카드2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // 1. 카드 개수 입력
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Queue<Integer> queue = new LinkedList<>();  // 일반 큐 사용
-        List<Integer> checkList = new ArrayList<>();
+        Map<Integer, Integer> cardCount = new HashMap<>();
 
-        int count = 0;
-        int number = st.countTokens();
-
-        // 큐에 숫자 추가
-        for (int i = 0; i < number; i++) {
-            queue.add(Integer.parseInt(st.nextToken()));
+        // 2. 카드 개수 카운팅
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            cardCount.put(num, cardCount.getOrDefault(num, 0) + 1);
         }
 
-        // 숫자 개수 세기
-        while (!queue.isEmpty()) {
-            int num = queue.poll();  // 큐에서 꺼내기
-            if (queue.contains(num)) {
-                count++;
-            } else {
-                checkList.add(count + 1);
-                count = 0;
-            }
-        }
+        // 3. 찾고 싶은 카드 개수 입력
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
-        // 결과 출력
-        for (int result : checkList) {
-            System.out.print(result + " ");
+        // 4. 결과 출력
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sb.append(cardCount.getOrDefault(num, 0)).append(" ");
         }
+        System.out.println(sb);
     }
 }
