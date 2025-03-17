1a261fff5945c1e782c5ddc7449dd34cec21a1a6
승엽 <tmdduqflfl@naver.com>
2025-03-17 18:47:24 +0900

    3/17 이해완

--- 맵/듣보잡.java
+++ 맵/듣보잡.java
@@ -3,8 +3,10 @@ package 맵;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -12,24 +14,25 @@ import java.util.StringTokenizer;
 public class 듣보잡 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int count =0;
-
+        Map<String, Integer> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Map<String, Integer> data = new HashMap<>();
-        List<String> names = List.of(new String[]{sb.toString()});
-
-        List<String> twoNames = List.of(new String[]{sb.toString()});
-
-        for (int i = 0; i <= names.size(); i++) {
-            if (twoNames.contains(names.get(i))) {
-                data.put(names.get(i), 1);
-                count+= data.get(i);
-            }
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            map.put(br.readLine(), 1);
         }
-
-        System.out.println(count);
-        System.out.println(data);
-
+        for (int i = 0; i < m; i++) {
+            String name = br.readLine();
+            map.put(name, map.getOrDefault(name,0) +  1);
+            if (map.get(name) == 2) list.add(name);
+        }
+        StringBuilder sb = new StringBuilder();
+        Collections.sort(list);
+        sb.append(list.size() + "\n");
+        for (String s : list) {
+            sb.append(s + "\n");
+        }
+        System.out.print(sb);
     }
 }
