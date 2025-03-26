f6c820ea1fa3d4152943e8b7fe8748b1a242914c
승엽 <tmdduqflfl@naver.com>
2025-03-26 18:09:04 +0900

    3/26 A-> B

---
+++ 그리디/AtoB.java
@@ -0,0 +1,7 @@
+package 그리디;
+
+public class AtoB {
+    public static void main(String[] args) {
+        
+    }
+}
--- 그리디/수들의합.java
+++ 그리디/수들의합.java
@@ -2,22 +2,37 @@ package 그리디;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
 public class 수들의합 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int sum = 0;
-        int count = 0;
-//        PriorityQueue<Integer> queue = new PriorityQueue<>();
-//        while (true){
-//            queue.add(count);
-//        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        sum=n/2;
-        System.out.println(sum);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+//2 162
+        int sum = 0;
+        int count = 1;
+        while (m != n) {
+            if (m < n) {
+                System.out.println(-1);
+                return;
+            }
+            String str = String.valueOf(m);
+            if (m % 2 == 0) {
+                m /= 2;
+            } else if (str.charAt(str.length() - 1) == '1') {
+                str = str.substring(0, str.length() - 1);
+                m=Integer.parseInt(str);
+            }
+            else {
+                System.out.println(-1);
+                return;
+            }
+        }
+
 
     }
 }
